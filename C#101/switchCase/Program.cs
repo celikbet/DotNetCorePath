ae047c5c2a7bac574d6f99044eb20eb570b15528
celikbet <celikbett@gmail.com>
2022-11-24 16:57:09 +0300

    SwitchCase subrepo added

---
+++ C#101/switchCase/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace switchCase {
+    public class Program {
+        static void Main(string[] args) 
+        {
+            int month = DateTime.Now.Month;
+
+            // Expression
+            switch (month)   
+            {
+                case 1:
+                    Console.WriteLine("In January");
+                    break;
+                case 2:
+                    Console.WriteLine("In February");
+                    break;
+                case 4:
+                    Console.WriteLine("In April");
+                    break;  
+                case 3:
+                    Console.WriteLine("In March");
+                    break;       
+                default:
+                    Console.WriteLine("Wrong data!");
+                break;
+            }
+
+
+            switch(month) 
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("The Winter is coming!");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("It is Spring!");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer Party!");
+                    break;  
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("It is Autumn!");
+                    break;                 
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ C#101/switchCase/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - Switch-Case](https://app.patika.dev/courses/csharp-101/2-switch-case)
+Switch-Case Konusu - Ödev
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
