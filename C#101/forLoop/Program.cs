3db03859667dfc5723d69bdfe728fe30d9ba70cb
celikbet <celikbett@gmail.com>
2022-11-24 21:41:11 +0300

    forLoop subrepo added

---
+++ C#101/forLoop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace forLoop {
+    public class Program {
+        static void Main(string[] args) 
+        {
+            // Count the all odd numbers to the number entered on terminal
+            Console.WriteLine("Please enter a number");
+            int count = int.Parse(Console.ReadLine());
+            for (int i=1; i<=count; i++) 
+            {
+                if(i%2 == 1) 
+                    Console.WriteLine(i);
+            }
+
+            // Calculate summation of add numbers & summation of even numbers within the range 1-1000
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            for (int i=1; i<=1000; i++) 
+            {
+                if (i%2 == 1) 
+                    sumOdd += i;
+                else
+                    sumEven += i;
+            }
+
+            Console.WriteLine("Summation of Even numbers: " + sumEven);
+            Console.WriteLine("Summation of Odd numbers: " + sumOdd);
+
+            // Use of Break && Continue
+            for (int i=1; i<10; i++) 
+            {
+                if(i==4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            for (int i=1; i<10; i++) 
+            {
+                if(i==4)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
---
+++ C#101/forLoop/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - ForLoopBreakContinue](https://app.patika.dev/courses/csharp-101/1-for-dongusu-break-continue)
+ForLoopBreakContinue Konusu - Ödev 1
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
