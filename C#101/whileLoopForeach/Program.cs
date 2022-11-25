b2d99a1e23c71d9929c5642efd626cb0b8abb68e
celikbet <celikbett@gmail.com>
2022-11-25 16:35:06 +0300

    whileLoopForeach subrepo added

---
+++ C#101/whileLoopForeach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace whileLoopForeach {
+    public class Program {
+        static void Main(string[] args) 
+        {
+            // Calculate average of the numbers
+            int number = int.Parse(Console.ReadLine());
+            int count = 1;
+            int summation = 0;
+
+            while (count <= number) 
+            {
+                summation+=count;
+                count++;
+            }
+            int average = summation/count;
+            Console.WriteLine(average);
+
+            // Print all the letters from a to z
+            char character = 'a';
+            while (character < 'z') 
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            //Foreach
+            string [] cars = {"BWM", "Volvo", "Toyota"};
+            foreach(var item in cars) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ C#101/whileLoopForeach/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - WhileLoopForeach](https://app.patika.dev/courses/csharp-101/2-while-foreach-donguleri)
+WhileLoop-Foreach Konusu - Ödev
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
