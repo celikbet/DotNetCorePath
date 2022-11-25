872ffbb82036be9b2d2907b217c42238b566b64e
celikbet <celikbett@gmail.com>
2022-11-25 17:07:14 +0300

    seriesWithLoops subrepo added

---
+++ C#101/seriesWithLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace seriesWithLoops {
+    public class Program {
+        static void Main(string[] args) 
+        {
+            // Definitions
+            string[] colors = new string[5];
+            string[] animals = {"cat", "dog", "bird", "monkey"};
+            int[] series1;
+            series1 = new int[5];
+
+            // Assign a value & Reach to value 
+            colors[0] = "blue";
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(animals[1]);
+            series1[3] = 10;
+            Console.WriteLine(series1[3]);
+
+            // Series with Loops
+            Console.WriteLine("Please enter the number of element!");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] numberSeries = new int[length];
+
+            for (int i=0; i<length; i++) 
+            {
+                Console.WriteLine("Please enter {0} th element's value: ", i+1);
+                numberSeries[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            int summation = 0;
+            foreach (var number in numberSeries) 
+            {
+                summation += number;
+            }
+            Console.WriteLine("Average: " + summation/length);
+
+        }
+    }
+}
---
+++ C#101/seriesWithLoops/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - SeriesWithLoops](https://app.patika.dev/courses/csharp-101/1-dizi-tan%C4%B1mlama-eris%CC%A7im-ve-do%CC%88ngu%CC%88lerle-kullan%C4%B1m)
+Series With Loops Konusu - Ödev
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
