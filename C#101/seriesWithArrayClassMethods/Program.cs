97fab59a28e299813caba3e061f5bf28cc702e2c
celikbet <celikbett@gmail.com>
2022-11-30 12:49:46 +0300

    seriesWithArrayClassMethods subrepo added

---
+++ C#101/seriesWithArrayClassMethods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace seriesWithArrayClassMethod {
+    public class Program {
+
+        static void Main(string[] args) {
+            // SORT (sorting array elements within order)
+            int[] numberArray = {23, 12, 4, 86, 72, 3, 11, 17};
+     
+            Console.WriteLine("***Array Without Order***");
+            foreach(var number in numberArray) {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("***Order Array***");
+            Array.Sort(numberArray);
+            foreach(var number in numberArray) {
+                Console.WriteLine(number);
+            }
+
+            // CLEAR (assign zero to the spesified element' value of an array)
+            Console.WriteLine("***Clear Array***");
+            Array.Clear(numberArray, 2, 2);
+            foreach(var number in numberArray) {
+                Console.WriteLine(number);
+            }      
+
+            // REVERSE (reverse order of array elements)
+            Console.WriteLine("***Reverse Array***");
+            Array.Reverse(numberArray);
+            foreach(var number in numberArray) {
+                Console.WriteLine(number);
+            }  
+
+            // INDEXOF (specify the index of element's value within array)
+            Console.WriteLine("***Index Of Array***");
+            Console.WriteLine(Array.IndexOf(numberArray, 23));
+
+            // RESIZE (resize the array length)
+            Console.WriteLine("***Resize Array***");
+            Array.Resize<int>(ref numberArray, 9);
+            numberArray[8] = 99;
+            foreach(var number in numberArray) {
+                Console.WriteLine(number);
+            }  
+            
+        }
+    }
+}
---
+++ C#101/seriesWithArrayClassMethods/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - SeriesWithArrayClassMethods](https://app.patika.dev/courses/csharp-101/2-array-sinifi-ve-methodlari)
+Series With Array Class Methods Konusu - Ödev
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
