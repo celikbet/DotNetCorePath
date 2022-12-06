f6759d50a285f7cbbc3cee0fb939df2544170998
celikbet <celikbett@gmail.com>
2022-12-06 17:02:06 +0300

    classInstanceFieldProperty  subrepo added

---
+++ C#101/classInstanceFieldProperty/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace classInstanceFieldProperty {
+
+    public class Program {
+
+        static void Main(string[] args) 
+        {
+            // INFO
+            //class ClassName
+            //{
+            //      [Access Modifiers] [Data Type] PropertyName
+            //      [Access Modifiers] [Return Type] MethodName([ParameterList])
+            //      {
+            //          // method commands
+            //      }
+            //}
+
+            // Access Modifiers: Public - Private - Internal - Protected
+
+
+            // Create an instance from Employee Class
+            Employee employeeOne = new Employee();
+            employeeOne.Name = "nameOne";
+            employeeOne.LastName = "lastNameOne";
+            employeeOne.Number = 2385599;
+            employeeOne.Department = "IT-Deparment";
+            employeeOne.EmployeeInformation();
+            Console.WriteLine("************************************");
+            Employee employeeTwo = new Employee();
+            employeeTwo.Name = "nameTwo";
+            employeeTwo.LastName = "lastNameTwo";
+            employeeTwo.Number = 2386699;
+            employeeTwo.Department = "Sales-Department";
+            employeeTwo.EmployeeInformation();
+
+
+        }
+
+        
+        class Employee 
+        {
+            public string Name;
+            public string LastName;
+            public int Number;
+            public string Department;
+
+            public void EmployeeInformation()
+            {
+                Console.WriteLine("Employee' Name: " + Name);
+                Console.WriteLine("Employee' LastName: " + LastName);
+                Console.WriteLine("Employee' Number: " + Number);
+                Console.WriteLine("Employee' Department: " + Department);
+
+
+            }
+
+        }
+
+    }
+
+}
---
+++ C#101/classInstanceFieldProperty/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - Class & Instance & Field & Property](https://app.patika.dev/courses/csharp-101/1-sinif-field-metot-tan%C4%B1mlama-eris%CC%A7im-belirleyiciler)
+C# 101 - Class & Instance & Field & Property - Homework
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
