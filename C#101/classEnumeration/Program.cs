3c815da5ba5ae9bcc425e0cd7d5e7c137099f77c
celikbet <celikbett@gmail.com>
2022-12-07 21:35:28 +0300

    classEnumeration subrepo added

---
+++ C#101/classEnumeration/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace classEnumeration {
+
+    class Program {
+
+        static void Main(string[] args) 
+        {
+            // Days Enumaration
+            Console.WriteLine(Days.Sunday);
+            Console.WriteLine(((int)Days.Saturday));
+
+            // Weather Enumaration
+            int temperature = 32;
+            if(temperature <= (int)Weather.Normal) 
+                Console.WriteLine("Wait for better weather to go outside");
+            else if (temperature  >= (int)Weather.Sunny)
+                Console.WriteLine("It is very warm to go outside");
+            else if (temperature >= (int)Weather.Normal && temperature < (int)Weather.VerySunny)
+                Console.WriteLine("Let's go outside!");
+
+
+        }
+    }
+
+    enum Days 
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesdey,
+        Thursday,
+        Friday=23,
+        Saturday
+    }
+
+    enum Weather
+    {
+        Cold = 5,
+        Normal = 20,
+        Sunny = 25,
+        VerySunny = 30,
+    }
+}
---
+++ C#101/classEnumeration/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - Class Enumeration](https://app.patika.dev/courses/csharp-101/17-enum)
+C# 101 - Class Enumeration - Homework
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
