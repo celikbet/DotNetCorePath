3d10b7c33edb5bfe80d5204899d259277086bc15
celikbet <celikbett@gmail.com>
2022-12-07 18:17:16 +0300

    classStruct subrepo added

---
+++ C#101/classStruct/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace classStruct {
+
+    class Program {
+
+        static void Main(string[] args) 
+        {
+            // Class
+            Console.WriteLine("***** Class *****");
+            Rectangle rectangleClass = new Rectangle();
+            Console.WriteLine("Calculation of Rectangle Area BY Class: {0}", rectangleClass.CalculateArea());
+
+            // Struct 
+            Console.WriteLine("***** Struct *****");
+            RectangleStruct rectangleStruct; // OR can be as "RectangleStruct rectangleStruct = new RectangleStruct();"
+            rectangleStruct.shortEdge = 3;
+            rectangleStruct.longEdge = 4;
+            Console.WriteLine("Calculation of Rectangle Area BY Struct: {0}", rectangleStruct.CalculateArea());
+
+        }
+    }
+
+    class Rectangle {
+        public int shortEdge;
+        public int longEdge;
+
+        public Rectangle() {
+            shortEdge = 3;
+            longEdge = 4;
+        }
+
+        public long CalculateArea() 
+        {
+            return this.shortEdge * this.longEdge;
+        }
+    }
+
+    struct RectangleStruct {
+        public int shortEdge;
+        public int longEdge;
+
+        public long CalculateArea() 
+        {
+            return this.shortEdge * this.longEdge;
+        }
+    }
+}
---
+++ C#101/classStruct/ReadMe.md
@@ -0,0 +1,4 @@
+## [C# 101 - Class & Struct](https://app.patika.dev/courses/csharp-101/5-struct-nedir)
+C# 101 - Class & Struct 
+
+[Hazırlayan: Betül Çelik Çetin](https://app.patika.dev/celikbet)
